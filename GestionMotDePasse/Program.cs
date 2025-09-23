ec9f47f5ccce1569c2e5bdbf36c9ec67b37e4f64
orabillon <115501074+orabillon@users.noreply.github.com>
2025-09-23 09:43:24 +0200

    Ajout gestion simple du mot de passe

--- GestionMotDePasse/Program.cs
+++ GestionMotDePasse/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using GestionMotDePasse.Data;
+using GestionMotDePasse.Data.Models;
 using Microsoft.EntityFrameworkCore;
 
 // gestion base sql lite
@@ -8,6 +9,8 @@ await using var context = new PassManagerDbContext();
 // création de la base de données si elle n'existe pas
 await context.Database.MigrateAsync();
 
+// grise le menu si pas de mot de passes
+bool isEmpty = !await context.Passwords.AnyAsync();
 
 string[] menuItems = ["Ajouter un mot de passe", "Consulter un mot de passe", "Quitter"];
 
@@ -20,11 +23,21 @@ string choix = "";
 
 do
 {
+    // vérifie si la base de données est vide
+    if (isEmpty)
+    {
+        isEmpty = !await context.Passwords.AnyAsync();
+    }
+
     Console.Clear();
 
     foreach ((int idx, string menuItem) in menuItems.Index())
     {
-        if(idx == index)
+        if (isEmpty && idx == 1)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+        }
+        else if (idx == index)
         {
             Console.BackgroundColor = ConsoleColor.Gray;
             Console.ForegroundColor = ConsoleColor.Black;
@@ -37,20 +50,60 @@ do
 
     if (key.Key == ConsoleKey.UpArrow)
     {
-        index--;
-        if (index < 0) index = menuItems.Length - 1;
+        do
+        {
+            index--;
+            if (index < 0) index = menuItems.Length - 1;
+        } while (isEmpty && index == 1);
     }
     else if (key.Key == ConsoleKey.DownArrow)
     {
-        index++;
-        if (index >= menuItems.Length) index = 0;
+        do
+        {
+            index++;
+            if (index >= menuItems.Length) index = 0;
+        } while (isEmpty && index == 1);
+
     }
     else if (key.Key == ConsoleKey.Enter)
     {
-        choix = menuItems[index];
-        Console.WriteLine(choix);
-        Console.WriteLine("Appuyez sur une touche pour continuer...");
-        Console.ReadLine();
+        switch (index)
+        {
+            case 0:
+                Console.WriteLine("Entrez le nom du mot de passe :");
+                string? name = Console.ReadLine();
+                while (string.IsNullOrEmpty(name))
+                {
+                   name = Console.ReadLine();
+                }
+
+                Console.WriteLine("Entrez le mot de passe :");
+                string? password = Console.ReadLine();
+                while (string.IsNullOrEmpty(password))
+                {
+                    password = Console.ReadLine();
+                }
+
+                var passwordEntry = new PasswordEntry()
+                {
+                    Name = name,
+                    Value = password,
+                    InsertDate = DateTime.UtcNow
+                };
+
+                context.Passwords.Add(passwordEntry);
+                await context.SaveChangesAsync();
+               
+                Console.WriteLine("Mot de passe ajouté avec succès !");
+                Console.WriteLine("Appuyez sur une touche pour continuer...");
+                Console.ReadLine();
+                break;
+
+
+            case 2:
+                Environment.Exit(0);
+                break;
+        }
     }
 }
 while (key.Key != ConsoleKey.Escape);
